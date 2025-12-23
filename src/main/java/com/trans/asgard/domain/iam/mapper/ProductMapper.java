63c2e984cde22b1d534320b3905678ed804006dd
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-12-23 11:06:57 +0100

    add ProductMapper interface for mapping between Product and ProductDto

---
+++ src/main/java/com/trans/asgard/domain/iam/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.trans.asgard.domain.iam.mapper;
+
+import com.trans.asgard.domain.iam.dto.ProductDto;
+import com.trans.asgard.domain.iam.model.Product;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    ProductDto toDto(Product product);
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Product toEntity(ProductDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(ProductDto dto, @MappingTarget Product entity);
+}
