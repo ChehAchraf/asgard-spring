65b6e1dc4d884847e70e91dc08c440a4daddeb90
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-12-23 11:06:44 +0100

    add ProductController with createProduct endpoint

---
+++ src/main/java/com/trans/asgard/domain/iam/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.trans.asgard.domain.iam.controller;
+
+
+import com.trans.asgard.domain.iam.dto.RegisterUserRequest;
+import com.trans.asgard.domain.iam.dto.UserResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+
+    @PostMapping
+    public ResponseEntity<> createProduct(@Valid @RequestBody RegisterUserRequest request)
+    {
+
+    }
+}
