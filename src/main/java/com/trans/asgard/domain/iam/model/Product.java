25339b87b32cc3ba956e24ad5617ec3743b6d431
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-12-23 11:07:06 +0100

    add Product entity with fields and JPA annotations

---
+++ src/main/java/com/trans/asgard/domain/iam/model/Product.java
@@ -0,0 +1,32 @@
+package com.trans.asgard.domain.iam.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = 'products')
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String description;
+    private String categorie;
+    private double prixVente;
+    private double prixAchat ;
+    private double marge ;
+    private double poids;
+    private String unite;
+
+
+//    @OneToMany(mappedBy="product" , cascade = CascadeType.ALL,orphanRemoval = true)
+//    private List<Stock> stocks;
+}
+
