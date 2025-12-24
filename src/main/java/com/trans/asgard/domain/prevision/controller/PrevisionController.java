c18f3118f566a245c527f13c11189bb16d6ff3e4
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-12-24 17:06:54 +0100

    feat : prevision controller added

---
+++ src/main/java/com/trans/asgard/domain/prevision/controller/PrevisionController.java
@@ -0,0 +1,30 @@
+package com.trans.asgard.domain.prevision.controller;
+
+import com.trans.asgard.domain.prevision.dto.PrevisionResponse;
+import com.trans.asgard.domain.prevision.dto.PrevisionTestRequest;
+import com.trans.asgard.domain.prevision.service.intefaces.PrevisionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/prevision")
+@RequiredArgsConstructor
+public class PrevisionController {
+
+    private final PrevisionService previsionService;
+
+    @PostMapping("/product/{productId}/warehouse/{warehouseId}")
+    public ResponseEntity<PrevisionResponse> generateForecast(
+            @PathVariable Long productId,
+            @PathVariable Long warehouseId) {
+        return ResponseEntity.ok(previsionService.generateAndSaveForecast(productId, warehouseId));
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<PrevisionResponse> generateTestForecast(
+            @Valid @RequestBody PrevisionTestRequest request) {
+        return ResponseEntity.ok(previsionService.generateAndSaveTestForecast(request));
+    }
+}
