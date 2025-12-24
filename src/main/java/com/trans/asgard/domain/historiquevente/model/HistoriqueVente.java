7b41372466cda00db9d5ba812899c1abd0036238
chehachraf <achrafchehboun@outlook.com>
2025-12-24 15:28:22 +0100

    add stock service with custom error handling exception

--- src/main/java/com/trans/asgard/domain/historiquevente/model/HistoriqueVente.java
+++ src/main/java/com/trans/asgard/domain/historiquevente/model/HistoriqueVente.java
@@ -4,6 +4,7 @@ import com.trans.asgard.domain.Entrepot.model.Entrepot;
 import com.trans.asgard.domain.product.model.Product;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "historiqueVente")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 
